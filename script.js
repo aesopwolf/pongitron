279e81fef965e2d59559c424961fb6ad0b455611
Aesop Wolf <aesopwolf@gmail.com>
2015-02-26 13:01:40 -0800

    removed commented code

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
 angular.module('app', [])
-// .directive('onKeyDown', function() {
-//   return {
-//     restrict: 'A',
-//     link: function(scope, elem, attrs) {
-//       // this next line will convert the string
-//       // function name into an actual function
-//       var functionToCall = scope.$eval(attrs.ngKeydown);
-//       elem.on('keydown', function(e){
-//         console.log(e);
-//         // on the keydown event, call my function
-//         // and pass it the keycode of the key
-//         // that was pressed
-//         // ex: if ENTER was pressed, e.which == 13
-//         // functionToCall(e.which);
-//       });
-//     }
-//   };
-// })
 .controller('MainController', function($scope, $rootScope, $document) {
   $document.bind('keyup', function(event) {
     $rootScope.$broadcast('keypress', event);
