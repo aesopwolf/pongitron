2fab8d13eecc53cad8e61d112d257a7b27c96a1d
Aesop Wolf <aesopwolf@gmail.com>
2015-02-25 18:09:55 -0800

    ui enhancements

--- public/script.js
+++ public/script.js
@@ -22,23 +22,45 @@ angular.module('app', [])
     $rootScope.$broadcast('keypress', event);
   });
 
+  $scope.flash1 = false;
+  $scope.flash2 = false;
+
   $scope.$on('keypress', function(n, event) {
-    console.log(event.keyCode);
     if(event.keyCode === 49) {
+      $scope.flash1 = true;
       $scope.player1.score += 1;
-      if($scope.player1.score > 9) {
+      if($scope.player1.score > 20) {
         $scope.player1.score = 0;
       }
       $scope.$apply();
+      var audio = new Audio('/ping.mp3');
+      audio.play();
+      if((($scope.player1.score + $scope.player2.score) % 5) === 0 ) {
+        var audio = new Audio('/switch.mp3');
+        audio.play();
+      }
     }
 
     if(event.keyCode === 86) {
+      $scope.flash2 = true;
       $scope.player2.score += 1;
-      if($scope.player2.score > 9) {
+      if($scope.player2.score > 20) {
         $scope.player2.score = 0;
       }
       $scope.$apply();
+      var audio = new Audio('/ping.mp3');
+      audio.play();
+      if((($scope.player1.score + $scope.player2.score) % 5) === 0 ) {
+        var audio = new Audio('/switch.mp3');
+        audio.play();
+      }
     }
+
+    setTimeout(function() {
+      $scope.flash1 = false;
+      $scope.flash2 = false;
+      $scope.$apply();
+    }, 1000);
   });
 
   $scope.player1 = {
