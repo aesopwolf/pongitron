f31f93a314c4d8673e89f0174905a7344e722aba
Aesop Wolf <aesopwolf@gmail.com>
2015-03-04 15:03:39 -0800

    feature: reset score by holding on the button

--- script.js
+++ script.js
@@ -3,41 +3,81 @@ var ping = new Audio('ping.mp3');
 
 angular.module('app', [])
 .controller('MainController', function($scope, $rootScope, $document) {
-  $document.bind('keyup', function(event) {
-    $rootScope.$broadcast('keypress', event);
-  });
+  // keybindings
+  /***************************************/
+  $document.bind('keyup', function(event) {$rootScope.$broadcast('keyup', event);});
+  $document.bind('keydown', function(event) {$rootScope.$broadcast('keydown', event);});
 
+  // variables
+  /***************************************/
+  var player1ResetCounter = 0;
+  var player1Reset = false;
+  var player2ResetCounter = 0;
+  var player2Reset = false;
+
+  $scope.player = {one: 0, two: 0};
   $scope.flash1 = false;
   $scope.flash2 = false;
 
-  $scope.$on('keypress', function(n, event) {
+  // reset the counter if they hold down the button
+  /***************************************/
+  $scope.$on('keydown', function(n, event) {
     if(event.keyCode === 49) {
-      $scope.flash1 = true;
-      $scope.player1.score += 1;
-      if($scope.player1.score > 20) {
-        $scope.player1.score = 0;
-      }
-      $scope.$apply();
-
-      ping.play();
-      if((($scope.player1.score + $scope.player2.score) % 5) === 0 ) {
-        switchService.play();
+      player1ResetCounter++;
+      if(player1ResetCounter === 10) {
+        ping.play();
+        player1Reset = true;
+        $scope.player.one = 0;
+        $scope.$apply();
       }
     }
-
     if(event.keyCode === 86) {
-      $scope.flash2 = true;
-      $scope.player2.score += 1;
-      if($scope.player2.score > 20) {
-        $scope.player2.score = 0;
-      }
-      $scope.$apply();
-      ping.play();
-      if((($scope.player1.score + $scope.player2.score) % 5) === 0 ) {
-        switchService.play();
+      player2ResetCounter++;
+      if(player2ResetCounter === 10) {
+        ping.play();
+        player2Reset = true;
+        $scope.player.two = 0;
+        $scope.$apply();
       }
     }
+  });
 
+  // increase the counter when they let go of the button
+  /***************************************/
+  $scope.$on('keyup', function(n, event) {
+    // player 1
+    if(event.keyCode === 49 && !player1Reset) {
+      ping.play();
+      $scope.flash1 = true;
+      $scope.player.one += 1;
+      if($scope.player.one > 20) {
+        $scope.player.one = 0;
+      }
+      $scope.$apply();
+      player1ResetCounter = 0;
+    }
+    else if(player1Reset) {
+      player1Reset = false;
+      player1ResetCounter = 0;
+    }
+
+    // player 2
+    if(event.keyCode === 86 && !player2Reset) {
+      ping.play();
+      $scope.flash1 = true;
+      $scope.player.two += 1;
+      if($scope.player.two > 20) {
+        $scope.player.two = 0;
+      }
+      $scope.$apply();
+      player2ResetCounter = 0;
+    }
+    else if(player2Reset) {
+      player2Reset = false;
+      player2ResetCounter = 0;
+    }
+
+    // turn the flash background off
     setTimeout(function() {
       $scope.flash1 = false;
       $scope.flash2 = false;
@@ -45,10 +85,16 @@ angular.module('app', [])
     }, 1000);
   });
 
-  $scope.player1 = {
-    score: 0
-  };
-  $scope.player2 = {
-    score: 0
-  };
+  // inform the players to switch service
+  /***************************************/
+  $scope.$watchCollection("player", function(player, oldValue) {
+    if(((player.one + player.two) % 5) === 0) {
+      if(player.one === 0 && player.two === 0 ) {
+        // new game!
+      }
+      else {
+        switchService.play();
+      }
+    }
+  });
 });
