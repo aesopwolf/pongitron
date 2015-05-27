c6601ab1b5c9a282e3d8fac08b27a779f29a7544
Aesop Wolf <aesopwolf@gmail.com>
2015-05-27 10:41:31 -0700

    update keycode

--- scripts/App.js
+++ scripts/App.js
@@ -132,7 +132,7 @@ export default class App extends React.Component {
     };
 
     // add score to player2
-    if(e.keyCode === 86) {
+    if(e.keyCode === 44) {
       ping.play();
       newScores.push({
         player1: player1,
